a0e976e1b67b93791b0645db3ce49a0a852537a5
Ilya <Sorcerer122@gmail.com>
2023-08-05 18:18:58 +0300

    Changing names

--- AudioCreator.py
+++ AudioCreator.py
@@ -12,9 +12,8 @@ class Word:
     TranslatedLang: str = ''
 
 
-def create_combined_audio(word: Word):
+def __CreateCombinedAudio(word: Word):
 
-    # Function to convert text to AudioSegment using gTTS
     def text_to_audio(text, lang):
         mp3_fp = BytesIO()
         tts = gTTS(text=text, lang=lang)
@@ -23,28 +22,23 @@ def create_combined_audio(word: Word):
         audio_segment = AudioSegment.from_mp3(mp3_fp)
         return audio_segment
 
-    # Generate Japanese word audio
-    japanese_audio = text_to_audio(word.Original, word.OriginalLang)
+    originalAudio = text_to_audio(word.Original, word.OriginalLang)
 
-    # Generate English word audio
-    english_audio = text_to_audio(word.Translated, word.TranslatedLang)
+    englishAudio = text_to_audio(word.Translated, word.TranslatedLang)
 
-    # Generate countdown audio
-    silence_3_sec = AudioSegment.silent(duration=1500)
-    silence_2_sec = AudioSegment.silent(duration=1500)
+    silence1500sec = AudioSegment.silent(duration=1500)
+    silence1500sec2 = AudioSegment.silent(duration=1500)
 
-    # Combine the three parts
-    combined_audio = japanese_audio + silence_3_sec + english_audio + silence_2_sec
+    combined_audio = originalAudio + silence1500sec + englishAudio + silence1500sec2
 
-    # Apply a 2-second fade-out
-    fade_out_duration = 2000  # in milliseconds
-    combined_audio_with_fade = combined_audio.fade_out(fade_out_duration)
+    fadeOutDur = 2000  # in milliseconds
+    combinedSound = combined_audio.fade_out(fadeOutDur)
 
-    return combined_audio_with_fade
+    return combinedSound
 
 
 def CreateAudio(words: list[Word], parentFolder='Words'):
     for word in words:
-        combined_audio = create_combined_audio(word)
+        combined_audio = __CreateCombinedAudio(word)
         combined_audio.export(
             f'{parentFolder}\\{word.Original}_{word.Translated}.mp3', format="mp3")
